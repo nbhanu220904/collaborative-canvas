11b88a21a66d047eeb0f1446f8dd1a6cffcc86df
nbhanu220904 <n.bhanudurgaprasad2209@gmail.com>
2026-02-01 23:21:21 +0530

    feat: Initialize Socket.IO client for real-time communication.

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
 // Ensure this matches your server port
-const URL = "http://localhost:5000";
+// const URL = "http://localhost:5000";
+const URL = "https://collaborative-canvas-backend.vercel.app/";
 
 export const socket = io(URL);
